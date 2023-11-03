7f7efd7e463f5fb2f5d7b660052545ccb8946a58
sukiboo <dereventsov@gmail.com>
2023-11-03 16:53:28 -0400

    change distribution parameter structure

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ from collections import defaultdict
 from target_functions import setup_optimization
 
 
+#TODO: logging
+#TODO: visualization
 class Experiment:
 
-    def __init__(self, params):
-        self.__dict__.update(params)
+    def __init__(self, exp_params):
+        self.__dict__.update(exp_params)
         self.setup_target_function()
 
     def setup_target_function(self):
@@ -18,9 +20,9 @@ class Experiment:
         self.logs = defaultdict(list)
         self.mc_rng = np.random.default_rng(seed=self.random_seed)
 
-    def optimize(self, dist, dist_params):
+    def optimize(self, dist, params):
         """Minimize target function with a given distribution."""
-        self.__dict__.update(dist_params)
+        self.__dict__.update(params)
         x = self.x0.copy()
         self.logs[dist].append(self.fun(x))
         print(f'Optimizing {self.dim}-{self.function_name} with {dist} distribution...')
@@ -36,7 +38,7 @@ class Experiment:
         if dist == 'normal':
             u = self.mc_rng.normal(size=(self.num_mc, self.dim))
         elif dist == 'uniform':
-            u = self.mc_rng.random(size=(self.num_mc, self.dim))
+            u = 2*self.mc_rng.random(size=(self.num_mc, self.dim)) - 1
         elif dist == 'cauchy':
             u = self.mc_rng.standard_cauchy(size=(self.num_mc, self.dim))
         elif dist == 'laplace':
@@ -57,21 +59,21 @@ if __name__ == '__main__':
 
     # setup experiment
     exp_params = {'function_name': 'sphere', 'dim': 100,
-                  'num_steps': 10000, 'num_mc': 1000, 'random_seed': 0}
+                  'num_steps': 10000, 'num_mc': 100, 'random_seed': 0}
     exp = Experiment(exp_params)
 
     # setup optimization parameters
-    distribution_parameters = {
-        'normal': {'sigma': 1., 'lr': 1e-3},
-        'uniform': {'sigma': 1., 'lr': 1e-3},
-        'cauchy': {'sigma': 1., 'lr': 1e-3},
-        'laplace': {'sigma': 1., 'lr': 1e-3},
-        'logistic': {'sigma': 1., 'lr': 1e-3},
-        't': {'sigma': 1., 'lr': 1e-3},
-        }
+    distribution_parameters = [
+        ['normal', {'sigma': 1., 'lr': 1e-3}],
+        ['uniform', {'sigma': 1., 'lr': 1e-3}],
+        ['cauchy', {'sigma': 1., 'lr': 1e-3}],
+        ['laplace', {'sigma': 1., 'lr': 1e-3}],
+        ['logistic', {'sigma': 1., 'lr': 1e-3}],
+        ['t', {'sigma': 1., 'lr': 1e-3}],
+        ]
 
     # optimize with different kernels
-    for dist, dist_params in distribution_parameters.items():
-        exp.optimize(dist=dist, dist_params=dist_params)
+    for dist_params in distribution_parameters:
+        exp.optimize(*dist_params)
     print(pd.DataFrame(exp.logs))
 
