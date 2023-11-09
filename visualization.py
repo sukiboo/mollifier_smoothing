8336bc497ee88e767c3f07c2f94b043d586fe4bc
sukiboo <dereventsov@gmail.com>
2023-11-09 15:31:27 -0500

    implement logs visualization

---
+++ visualization.py
@@ -0,0 +1,80 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_theme(style='darkgrid', palette='muted', font='monospace', font_scale=1.)
+
+
+def visualize(logfile, percentile=(.25,.5,.75), show=True):
+    '''Visualize optimization results.'''
+    # read logs and aggregate statistics over multiple runs
+    df = pd.read_csv(logfile, index_col=0)
+    df_min = df.groupby(lambda x: x.split('|')[0], axis=1).quantile(percentile[0])
+    df_med = df.groupby(lambda x: x.split('|')[0], axis=1).quantile(percentile[1])
+    df_max = df.groupby(lambda x: x.split('|')[0], axis=1).quantile(percentile[2])
+
+    # plot median and confidence interval for each algorithm
+    fig, ax = plt.subplots(figsize=(8,5))
+    algs = df_med.columns.to_list()
+    for alg in algs:
+        plt.plot(df_med[alg], linewidth=4, alpha=.9, label=alg)
+        plt.fill_between(range(len(df_med)), df_min[alg], df_max[alg], alpha=.25)
+
+    # configure axis
+    ax.set_ylim(df.min().min(), df.loc[0].max())
+    ax.set_title(f"{logfile.split('/')[-1].split('.')[0]}")
+    plt.legend()
+    plt.tight_layout()
+
+    # save the results
+    os.makedirs('./images/', exist_ok=True)
+    savename = logfile.split('/')[-1].split('.')[0]
+    plt.savefig(f'./images/{savename}.png', dpi=300, format='png')
+    if show:
+        plt.show()
+    else:
+        plt.close()
+
+
+def visualize_search(logfile, percentile=(.25,.5,.75), show=True):
+    '''Visualize results of hyperparameter search.'''
+    # read logs and aggregate statistics over multiple runs
+    df = pd.read_csv(logfile, index_col=0)
+    df_min = df.groupby(lambda x: x.split('|')[0], axis=1).quantile(percentile[0])
+    df_med = df.groupby(lambda x: x.split('|')[0], axis=1).quantile(percentile[1])
+    df_max = df.groupby(lambda x: x.split('|')[0], axis=1).quantile(percentile[2])
+    T = len(df_med)
+
+    # plot median and confidence interval for each algorithm
+    sns.set_palette(sns.color_palette(palette='hls', n_colors=56))
+    fig, ax = plt.subplots(figsize=(12,6))
+    algs = df_med.columns.to_list()
+    for alg in algs:
+        plt.plot(df_med[alg], linewidth=4, alpha=.9, label=f'{alg} ({df_med[alg][T-1]:.2e})')
+        plt.fill_between(range(T), df_min[alg], df_max[alg], alpha=.25)
+
+    # configure axis
+    ax.set_ylim(df.min().min(), df.loc[0].max())
+    ax.set_title(f"{logfile.split('/')[-1].split('.')[0]}")
+    plt.legend(ncol=4, loc='upper center', bbox_to_anchor=(.5, 1.8))
+
+    # save the results
+    os.makedirs('./images/search/', exist_ok=True)
+    savename = logfile.split('/')[-1].split('.')[0]
+    plt.savefig(f'./images/search/{savename}.png', dpi=300, format='png', bbox_inches='tight')
+    if show:
+        plt.show()
+    else:
+        plt.close()
+
+
+if __name__ == '__main__':
+
+    # visualize each log file
+    logdir = './logs/'
+    for logfile in sorted(os.listdir(logdir)):
+        ##visualize(logdir + logfile, show=True)
+        visualize_search(logdir + logfile, show=False)
+
