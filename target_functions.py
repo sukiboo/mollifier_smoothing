2e9d8e5accb6887a6d7e5b8c70048846f13ba7b1
sukiboo <dereventsov@gmail.com>
2023-11-20 17:42:57 -0500

    implement target functions using numexpr

--- target_functions.py
+++ target_functions.py
@@ -8,61 +8,134 @@
 """
 
 import numpy as np
+try:
+    import numexpr as ne
+    use_numexpr = True
+except:
+    print('Not using numexpr, functions are going to be evaluated with numpy.')
+    use_numexpr = False
 
 
 def target_function(function_name, dim):
     """Setup benchmark function."""
     # Ackley function
     if function_name == 'ackley':
-        fun = lambda x: -20 * np.exp(-.2 * np.sqrt(np.mean(x**2, axis=1)))\
-            - np.exp(np.mean(np.cos(2*np.pi*x), axis=1)) + 20 + np.exp(1)
+        if use_numexpr:
+            def fun(x):
+                a = 20
+                b = .2
+                c = 2*np.pi
+                _dim = dim
+                mean1 = ne.evaluate('sum(x**2 / _dim, axis=1)')
+                mean2 = ne.evaluate('sum(cos(c*x) / _dim, axis=1)')
+                val = ne.evaluate('-a * exp(-b * mean1**.5) - exp(mean2) + a + exp(1)')
+                return val
+        else:
+            a = 20
+            b = .2
+            c = 2*np.pi
+            fun = lambda x: -a * np.exp(-b * np.sqrt(np.mean(x**2, axis=1)))\
+                - np.exp(np.mean(np.cos(c*x), axis=1)) + a + np.exp(1)
         x_dom = [[-32.768, 32.768]] * dim
         x_min = [0] * dim
 
     # Griewank function
     elif function_name == 'griewank':
-        fun = lambda x: np.sum(x**2, axis=1)/4000\
-            - np.prod(np.cos(x/np.sqrt(np.arange(dim)+1)), axis=1) + 1
+        if use_numexpr:
+            def fun(x):
+                weights = 1 + np.arange(dim)
+                sum1 = ne.evaluate('sum(x**2, axis=1)')
+                prod1 = ne.evaluate('prod(cos(x / weights**.5), axis=1)')
+                val = sum1 / 4000 - prod1 + 1
+                return val
+        else:
+            weights = 1 + np.arange(dim)
+            fun = lambda x: np.sum(x**2, axis=1) / 4000\
+                - np.prod(np.cos(x/np.sqrt(weights)), axis=1) + 1
         x_dom = [[-600, 600]] * dim
         x_min = [0] * dim
 
     # Levy function
     elif function_name == 'levy':
-        w = lambda x: (x + 3) / 4
-        fun = lambda x: np.sin(np.pi*w(x)[:,0])**2\
-            + np.sum((w(x)[:,:-1] - 1)**2 * (1 + 10*np.sin(np.pi*w(x)[:,:-1] + 1)**2), axis=1)\
-            + (w(x)[:,-1] - 1)**2 * (1 + np.sin(2*np.pi*w(x)[:,-1])**2)
+        if use_numexpr:
+            def fun(x):
+                w = (x.T + 3) / 4
+                w1 = w[0]
+                wi = w[:-1]
+                wd = w[-1]
+                pi = np.pi
+                first = ne.evaluate('sin(pi*w1)**2')
+                mid = ne.evaluate('sum((wi - 1)**2 * (1 + 10*sin(pi*wi + 1)**2), axis=0)')
+                last = ne.evaluate('(wd - 1)**2 * (1 + sin(2*pi*wd)**2)')
+                val = first + mid + last
+                return val
+        else:
+            w = lambda x: (x + 3) / 4
+            fun = lambda x: np.sin(np.pi*w(x)[:,0])**2\
+                + np.sum((w(x)[:,:-1] - 1)**2 * (1 + 10*np.sin(np.pi*w(x)[:,:-1] + 1)**2), axis=1)\
+                + (w(x)[:,-1] - 1)**2 * (1 + np.sin(2*np.pi*w(x)[:,-1])**2)
         x_dom = [[-10, 10]] * dim
         x_min = [1] * dim
 
     # Michalewicz function
     elif function_name == 'michalewicz':
-        m = 10 # default parameter
-        fun = lambda x: -np.sum(np.sin(x) * np.sin(np.arange(1,dim+1) * x**2 / np.pi)**(2*m), axis=1)
+        if use_numexpr:
+            def fun(x):
+                m = 10
+                pi = np.pi
+                weights = 1 + np.arange(dim)
+                val = -np.sum(ne.evaluate(f'sin(x) * sin(weights * x**2 / pi)**(2*m)'), axis=1)
+                return val
+        else:
+            m = 10
+            weights = 1 + np.arange(dim)
+            fun = lambda x: -np.sum(np.sin(x) * np.sin(weights * x**2 / np.pi)**(2*m), axis=1)
         x_dom = [[0, np.pi]] * dim
         x_min = [None] * dim
 
     # Rastrigin function
     elif function_name == 'rastrigin':
-        fun = lambda x: 10*dim + np.sum(x**2 - 10*np.cos(2*np.pi*x), axis=1)
+        if use_numexpr:
+            def fun(x):
+                pi = np.pi
+                val = 10*dim + ne.evaluate('sum(x**2 - 10*cos(2*pi*x), axis=1)')
+                return val
+        else:
+            fun = lambda x: 10*dim + np.sum(x**2 - 10*np.cos(2*np.pi*x), axis=1)
         x_dom = [[-5.12, 5.12]] * dim
         x_min = [0] * dim
 
     # Rosenbrock function
     elif function_name == 'rosenbrock':
-        fun = lambda x: np.sum(100 * (x[:,1:] - x[:,:-1]**2)**2 + (x[:,:-1] - 1)**2, axis=1)
+        if use_numexpr:
+            def fun(x):
+                xi, xj = x[:,1:], x[:,:-1]
+                val = ne.evaluate('sum(100*(xj - xi**2)**2 + (xi - 1)**2, axis=1)')
+                return val
+        else:
+            fun = lambda x: np.sum(100 * (x[:,1:] - x[:,:-1]**2)**2 + (x[:,:-1] - 1)**2, axis=1)
         x_dom = [[-5, 10]] * dim
         x_min = [1] * dim
 
     # Schwefel function -- taking absolute value to ensure the global minimum existance
     elif function_name == 'schwefel':
-        fun = lambda x: np.abs(418.9829*dim - np.sum(x * np.sin(np.sqrt(np.abs(x))), axis=1))
+        if use_numexpr:
+            def fun(x):
+                val = np.abs(418.9829*dim + ne.evaluate('sum(x * sin(abs(x)**.5), axis=1)'))
+                return val
+        else:
+            fun = lambda x: np.abs(418.9829*dim - np.sum(x * np.sin(np.sqrt(np.abs(x))), axis=1))
         x_dom = [[-500, 500]] * dim
         x_min = [420.9687] * dim
 
     # sphere function
     elif function_name == 'sphere':
-        fun = lambda x: np.sum(x**2, axis=1)
+        if use_numexpr:
+            def fun(x):
+                val = ne.evaluate('sum(x**2, axis=1)')
+                return val
+        else:
+            fun = lambda x: np.sum(x**2, axis=1)
         x_dom = [[-5.12, 5.12]] * dim
         x_min = [0] * dim
 
